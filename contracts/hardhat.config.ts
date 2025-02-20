29faef85a127306fb961097fd8715060cbcf2e4b
Ben Hooper <ben@labrys.io>
2025-02-20 13:28:32 +1000

    chore: explicitly added viem

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -5,7 +5,12 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      viaIR: true,
+    },
+  },
 };
 
 export default config;
