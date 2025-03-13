8f8dae9bb48378683196a83298d6d90482a07802
sethu <sethumadh@gmail.com>
2025-03-13 14:16:15 +1000

    update summary row

--- client/src/components/staking/staking-summary.tsx
+++ client/src/components/staking/staking-summary.tsx
@@ -10,21 +10,22 @@ function StakingSummary({
   tokensToStake,
   lockupDuration,
 }: StakingSummaryProps) {
+  const factor = 0.99 * (lockupDuration);
   return (
     <>
       <h2 className="font-dm_sans text-[16px] font-medium leading-[24px] text-black md:text-[18px] md:leading-[28px]">
         Summary
       </h2>
       <div className="flex flex-col">
-        <SummaryRow label="Token to be locked" value={tokensToStake} />
-        <SummaryRow label="Factor" value="0.99x" />
+        <SummaryRow label="Token to be locked" tokenValue={tokensToStake} />
+        <SummaryRow label="Factor" factorValue={`${factor}x`} />
         <SummaryRow
           label="Lock-up Duration"
-          value={`${lockupDuration} weeks`}
+          lockupDuration={`${lockupDuration} weeks`}
         />
         <SummaryRow
           label="Unlock Date"
-          value={calculateUnlockDate(lockupDuration)}
+          unlockDate={calculateUnlockDate(lockupDuration)}
         />
       </div>
     </>
--- client/src/components/staking/summary-row.tsx
+++ client/src/components/staking/summary-row.tsx
@@ -1,17 +1,37 @@
 
 interface SummaryRowProps {
     label: string;
-    value: string | number;
+    tokenValue?: string | number;
+    factorValue?: string | number;
+    lockupDuration?: string | number;
+    unlockDate?: string;
   }
   
-  const SummaryRow = ({ label, value }: SummaryRowProps) => (
+  const SummaryRow = ({ label, tokenValue, factorValue, lockupDuration, unlockDate }: SummaryRowProps) => (
     <div className="flex justify-between py-1 md:py-2">
       <span className="font-dm_sans text-[14px] leading-[20px] text-gray-500 md:text-[16px] md:leading-[24px]">
         {label}
       </span>
-      <span className="font-dm_sans text-[14px] leading-[20px] text-black md:text-[16px] md:leading-[24px]">
-        {value}
-      </span>
+      {tokenValue && (  
+        <span className="font-dm_sans text-[14px] leading-[20px] text-black md:text-[16px] md:leading-[24px]">
+          {tokenValue}
+        </span>
+      )}
+      {factorValue && (
+        <span className="font-dm_sans text-[14px] leading-[20px] text-black md:text-[16px] md:leading-[24px]">
+          {factorValue}
+        </span>
+      )}
+      {lockupDuration && (
+        <span className="font-dm_sans text-[14px] leading-[20px] text-black md:text-[16px] md:leading-[24px]">
+          {lockupDuration}
+        </span>
+      )}
+      {unlockDate && (
+        <span className="font-dm_sans text-[14px] leading-[20px] text-black md:text-[16px] md:leading-[24px]">
+          {unlockDate}
+        </span>
+      )}
     </div>
   );
 
