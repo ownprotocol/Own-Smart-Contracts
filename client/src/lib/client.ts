49620465090e65f6b15961b1297758e46d695cb7
sethu <sethumadh@gmail.com>
2025-03-17 14:46:37 +1000

    cleanup

--- client/src/lib/client.ts
+++ client/src/lib/client.ts
@@ -1,10 +1,9 @@
-// lib/client.ts
 import { createThirdwebClient } from "thirdweb";
 import { inAppWallet } from "thirdweb/wallets";
 import { createWallet } from "thirdweb/wallets";
 
-const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!; // this will be used on the client
-const secretKey = process.env.THIRDWEB_SECRET_KEY!; // this will be used on the server-side
+const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!;
+const secretKey = process.env.THIRDWEB_SECRET_KEY!;
 
 export const client = createThirdwebClient(
   secretKey ? { secretKey } : { clientId },
