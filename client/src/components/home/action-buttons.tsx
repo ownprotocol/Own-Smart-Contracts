c937a307cf1477fd2947c9a0c9fd5ea28566a2d5
sethu <sethumadh@gmail.com>
2025-03-17 15:30:16 +1000

    integrate fiat on ramp

--- client/src/components/home/action-buttons.tsx
+++ client/src/components/home/action-buttons.tsx
@@ -1,15 +1,42 @@
 "use client";
+import WertWidget from "@wert-io/widget-initializer";
+import type { Options } from "@wert-io/widget-initializer/types";
 
 import { Button } from "@/components/ui/button";
+import { activeChain } from "@/config/chain";
 
 function ActionButtons() {
+  // Define the options but don't initialize the widget yet
+  const getWertOptions = (): Options => ({
+    partner_id: "default",
+    // click_id: uuidv4(), // unique id of purсhase in your system
+    origin: "https://sandbox.wert.io", // this option needed only in sandbox
+    commodity: "ETH",
+    network: activeChain.name,
+    commodities: JSON.stringify([
+      {
+        commodity: "POL",
+        network: "amoy",
+      },
+      {
+        commodity: "ETH",
+        network: "sepolia",
+      },
+    ]),
+    listeners: {
+      loaded: () => console.log("loaded"),
+    },
+  });
+
   return (
     <div className="mt-4 flex flex-col gap-3 p-4 md:flex-row md:justify-center md:gap-4">
       <Button
         className="font-funnel bg-[#C58BFF] px-8 py-6 text-[14px] font-medium leading-[14px] tracking-[0%] text-black hover:bg-[#E49048] md:text-[16px] md:leading-[16px]"
         onClick={() => {
-          // Handle credit card payment
+          // Initialize the widget only when the button is clicked
+          const wertWidget = new WertWidget(getWertOptions());
           console.log("Credit card payment clicked");
+          wertWidget.open();
         }}
       >
         Buy with Card
