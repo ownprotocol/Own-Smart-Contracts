6cba6345116fbbe6ca9359202db4d9ff8af511b4
sethu <sethumadh@gmail.com>
2025-03-24 10:43:10 +1000

    feat: autheticate user on positions  page

--- client/src/app/positions/page.tsx
+++ client/src/app/positions/page.tsx
@@ -1,7 +1,48 @@
-import { MainNavigation } from "@/components";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useActiveAccount } from "thirdweb/react";
+
+import {
+  BlurredStakingBoard,
+  ConnectWalletButton,
+  MainNavigation,
+} from "@/components";
 import { StakePageContent } from "@/components/user-stake/stake-page-content";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { DialogHeader } from "@/components/ui/dialog";
+import { DialogContent } from "@/components/ui/dialog";
+import { Dialog } from "@/components/ui/dialog";
 
 function UserStakingPositionsPage() {
+  const router = useRouter();
+  const activeAccount = useActiveAccount();
+
+  const handleDialogClose = () => {
+    router.push("/");
+  };
+  if (!activeAccount?.address) {
+    return (
+      <main className="min-h-screen px-[5%] pt-[10%] md:px-[10%] md:pt-[3%]">
+        <Dialog
+          defaultOpen
+          onOpenChange={(open) => !open && handleDialogClose()}
+        >
+          <DialogContent className="border-gray-800 bg-[#141019] backdrop-blur-2xl sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="text-center text-white">
+                Connect Wallet to View Rewards
+              </DialogTitle>
+            </DialogHeader>
+            <div className="flex justify-center py-4">
+              <ConnectWalletButton redirectTo={`/positions`} />
+            </div>
+          </DialogContent>
+        </Dialog>
+        <BlurredStakingBoard />
+      </main>
+    );
+  }
   return (
     <main className="min-h-screen px-[5%] pt-[10%] md:px-[10%] md:pt-[3%]">
       <div className="relative flex flex-col">
