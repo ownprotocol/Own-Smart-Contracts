11d2a64cc31dff5f1d0192a89fdc1e1a4f882bbb
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-05-30 15:04:39 +1000

    fix: comment

--- contracts/deploy/005_local_testing.ts
+++ contracts/deploy/005_local_testing.ts
@@ -43,6 +43,7 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
   const rounds = Array.from({ length: 30 }, (_, i) => i).map(() => ({
     duration: 86400n,
+    // TODO: Price should be parseUnits("1.5", 6) for USDC
     price: parseEther("1.5"),
     allocation: parseEther("1000"),
     sales: 0n,
